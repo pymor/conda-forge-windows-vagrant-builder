513502b5b3696144c847b7d4e6dbd58bf02c3967
Peter Williams <peter@newton.cx>
2018-08-24 22:21:15 -0400

    build.ps1: work if there is only a "win_.yaml" file, but still prefer vs2015 if it exists

--- build.ps1
+++ build.ps1
@@ -10,6 +10,12 @@ $env:Path += ";c:\tools\miniconda3\Scripts"
 
 $feedstock = $feedstock -replace '/', '\\'
 cd c:\$feedstock
-$config = @(Get-ChildItem .ci_support/win*vs2015*.yaml)[0]
+$yamls = @(Get-ChildItem .ci_support/win*vs2015*.yaml)
+
+if ($yamls.Length > 0) {
+   $config = $yamls[0]
+} else {
+   $config = @(Get-ChildItem .ci_support/win*.yaml)[0]
+}
 
 conda build recipe -m c:\tools\miniconda3\conda_build_config.yaml -m $config
