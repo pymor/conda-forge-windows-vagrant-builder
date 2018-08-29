c6d0ed4319897bf3867cec829c4c3c330acd1948
Peter Williams <peter@newton.cx>
2018-08-28 23:21:06 -0400

    provision.ps1: whoa hey, we need VS2015 not VS2017 for conda

--- provision.ps1
+++ provision.ps1
@@ -33,8 +33,12 @@ conda update --all -y
 conda install -y conda-build conda-forge-pinning m2-bzip2 m2-vim posix
 conda clean -tipsy
 
-# Finally, Visual Studio build tools. Note that with the "right" parameters
-# this install can easily take an extremely long time and oodles of disk
-# space.
+# Finally, Visual Studio 2015 build tools. Note that with the "right"
+# parameters this install can easily take an extremely long time and oodles of
+# disk space.
 
-choco install visualstudio2017buildtools --package-parameters "--passive --locale en-US --add Microsoft.VisualStudio.Workload.VCTools"
+choco install vcbuildtools --package-parameters "/InstallSelectableItems NativeLanguageSupport_VC"
+
+# VS2017:
+# choco install visualstudio2017buildtools --package-parameters
+#   "--passive --locale en-US --add Microsoft.VisualStudio.Workload.VCTools"
