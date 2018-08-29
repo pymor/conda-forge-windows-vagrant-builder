8484b548aea2b488656cd6ff0d7aefb80d5039d8
Peter Williams <peter@newton.cx>
2018-08-29 02:15:29 -0400

    build.ps1: need a hack to get things working
    The right registry key doesn't get set.

--- build.ps1
+++ build.ps1
@@ -16,4 +16,6 @@ if ($yamls.Length -gt 0) {
    $config = @(Get-ChildItem .ci_support/win*.yaml)[0]
 }
 
+$env:VS140COMNTOOLS = "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\"
+
 conda build recipe -m $config
