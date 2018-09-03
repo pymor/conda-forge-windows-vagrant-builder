6715a8a7667bfa874687e423c24913cb9864a2ae
Peter Williams <peter@newton.cx>
2018-09-03 17:59:51 -0400

    condabash.bat: start in conda-bld, which is usually what I want

--- condabash.bat
+++ condabash.bat
@@ -1,4 +1,5 @@
 @REM Start a pretty-fully-loaded bash shell using the Conda MSYS2 environment.
 @ECHO OFF
+cd C:\Tools\Miniconda3\conda-bld
 SET PATH=C:\Tools\Miniconda3\Library\usr\bin;%PATH%
 bash
